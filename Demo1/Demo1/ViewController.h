a243239ff046e75e813093005c1a9de40fa81192
David Santana Molina <david@2coders.com>
2015-03-20 15:02:23 +0000

    Initial commit starting project

---
+++ Demo1/Demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo1
+//
+//  Created by David Santana Molina on 20/03/15.
+//  Copyright (c) 2015 2Coders Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo1/Demo1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo1
+//
+//  Created by David Santana Molina on 20/03/15.
+//  Copyright (c) 2015 2Coders Studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
